0a4b260a541256b44d4129ca1294c17ef1d43045
Jeirc-X <jericx@jericx.xyz>
2026-07-09 10:49:42 +0800

    简化：统一文本设置逻辑，移除Linux特定处理

--- src/SyncClipboard.Desktop/ClipboardAva/TextClipboardSetter.cs
+++ src/SyncClipboard.Desktop/ClipboardAva/TextClipboardSetter.cs
@@ -20,19 +20,7 @@ internal class TextClipboardSetter : ClipboardSetterBase<TextProfile>
         var text = metaInfomation?.Text ?? "";
         var item = new DataTransferItem();
 
-        if (OperatingSystem.IsLinux())
-        {
-            var utf8Text = Encoding.UTF8.GetBytes(text);
-            item.Set(DataFormat.CreateBytesPlatformFormat(Format.TEXT), utf8Text);
-            item.Set(DataFormat.CreateBytesPlatformFormat("text/plain"), utf8Text);
-            item.Set(DataFormat.CreateBytesPlatformFormat("text/plain;charset=utf-8"), utf8Text);
-            item.Set(DataFormat.CreateStringPlatformFormat(Format.Utf8String), text);
-        }
-        else
-        {
-            // macOS and Windows use simpler text format
-            item.SetText(text);
-        }
+        item.SetText(text);
 
         dataTransfer.Add(item);
         return Task.CompletedTask;
